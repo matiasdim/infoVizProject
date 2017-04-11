3b1b5f24b814d63d91eb123b612884b88b0616a1
Matías <matiasdim3@gmail.com>
2017-04-10 22:05:25 -0400

    .

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,14 @@
 class IndexController < ApplicationController
   def home
 
+  end
+
+  def top_ten_avg
 
   end
+
+  def main_att_detail
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'index#home'
+
+  get '/top_ten_avg' => 'index#top_ten_avg'
+  get '/main_att_detail' => 'index#main_att_detail'
 end
